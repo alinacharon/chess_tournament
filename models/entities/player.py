87dcf68b48c5536b666d3eab8f5a27fb9ca54580
Alina Charon <alina.charon@gmail.com>
2024-06-24 14:17:00 +0200

    Update: Player.name (last name before first name)

--- models/entities/player.py
+++ models/entities/player.py
@@ -16,7 +16,7 @@ class Player:
 
     @property
     def name(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.last_name} {self.first_name}'
 
     def __str__(self):
         return f"{self.name} ({self.player_id}), born {self.birthdate}, total points: {self.total_points}"
